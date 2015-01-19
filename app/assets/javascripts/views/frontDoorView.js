3d0a6b1bb1e8f95aff77f22d90d4cbef5001b96c
Evan Turner <evturn@gmail.com>
2015-01-19 00:17:08 -0500

    bootstrap wrapping all the content

--- app/assets/javascripts/views/frontDoorView.js
+++ app/assets/javascripts/views/frontDoorView.js
@@ -1,6 +1,6 @@
 var FrontDoorView = Backbone.View.extend({
 	el: '#games',
-	className: 'col-md-4 col-offset-2',
+	className: 'col-xs-5 col-xs-offset-1 col-md-5 col-md-offset-1',
 	template: _.template($('#hangman').html()),
 	initialize: function() {
 		this.render();
