bfe2a32d11199cd63194960f5d99471049bf158a
Evan Turner <evturn@gmail.com>
2015-01-18 20:40:31 -0500

    added custom images to assets

--- app/assets/javascripts/views/frontDoorView.js
+++ app/assets/javascripts/views/frontDoorView.js
@@ -1,5 +1,11 @@
 var FrontDoorView = Backbone.View.extend({
+	el: '#games',
+	className: 'col-md-4 col-offset-2',
+	template: _.template($('#hangman').html()),
 	initialize: function() {
 		this.render();
+	},
+	render: function() {
+		this.$el
 	}
 });
